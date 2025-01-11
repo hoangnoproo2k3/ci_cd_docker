11fcc682773753dd93a16ca3551aaea40be932c8
hoangviet <hoangnv@cyberk.io>
2025-01-11 15:12:31 +0700

    Refine CI/CD workflow conditions for backend and frontend builds; update greeting message in AppService

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, test ci cd';
+    return 'Hello, test ci cdc fwe2';
   }
 }
